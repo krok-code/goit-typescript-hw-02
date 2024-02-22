d637c93c6622c3a9c440730f6d4a81970594485f
krok-code <maks.smolinskyi@gmail.com>
2024-02-22 23:51:10 +0200

    update generatic

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,36 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+function compare<T extends AllType, K extends AllType>(
+  top: Pick<T, keyof AllType>,
+  bottom: Pick<K, keyof AllType>
+): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
+
+const topObj: Pick<AllType, keyof AllType> = {
+  name: "Top Name",
+  color: "Top Color",
+  position: 0,
+  weight: 0,
+};
+
+const bottomObj: Pick<AllType, keyof AllType> = {
+  position: 42,
+  weight: 100,
+  name: "",
+  color: "",
+};
+
+const result: AllType = compare(topObj, bottomObj);
+console.log(result);
 
 export {};
